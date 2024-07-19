bfbde28e11d1170145ffc82450ea5d2ae97e436d
AnassEREKYSY <ereanass@gmail.com>
2024-07-19 09:20:05 +0200

    CI-CD implementation

--- src/main/java/com/example/projetTechnique/ProjetTechniqueApplication.java
+++ src/main/java/com/example/projetTechnique/ProjetTechniqueApplication.java
@@ -1,12 +1,20 @@
 package com.example.projetTechnique;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class ProjetTechniqueApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		for (DotenvEntry entry : dotenv.entries()) {
+			System.setProperty(entry.getKey(), entry.getValue());
+		}
 		SpringApplication.run(ProjetTechniqueApplication.class, args);
 	}
 
